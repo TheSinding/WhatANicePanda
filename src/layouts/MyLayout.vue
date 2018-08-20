282a6ae0c7b2fc065d8a8047dd7d390641b9d436
Simon Sinding <sinding2000@gmail.com>
2018-08-20 16:26:45 +0200

    styling mostly

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,39 +1,54 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-layout-header>
-      <q-toolbar
-        color="primary">
-        <q-toolbar-title>
-          Randomly nice whale
-        </q-toolbar-title>
-      </q-toolbar>
+    <q-layout-header class="q-pt-md">
+         <p color="white" class="text-center text-white q-display-3 font-logo">Randomly nice Panda <q-icon name="fas fa-heart" color="red"/></p>
     </q-layout-header>
     <q-page-container>
       <router-view />
     </q-page-container>
     <q-layout-footer class="q-pa-md">
-      <q-icon name="fas fa-child" color="white" size="32px"/>
-      <p class="absolute-center text-white q-headline" >Press <b>spacebar</b> for a new randomly nice thing <q-icon color="red" name="fas fa-heart" /> </p>
-      <q-icon name="fas fa-plus float-right" color="white" size="32px"/>
-
+      <q-icon name="fas fa-child cursor-pointer" color="dark" size="32px">
+        <q-tooltip anchor="center right" self="center left">
+          About Randomly Nice
+        </q-tooltip>
+      </q-icon>
+      <p class="absolute-center text-dark q-headline font-text" >Press <b>spacebar</b> for a new randomly nice thing <q-icon color="red" name="fas fa-heart" /> </p>
+      <q-icon @click.native="$refs.teachingModal.show()" name="fas fa-plus float-right cursor-pointer" color="dark" size="32px">
+        <q-tooltip anchor="center left" self="center right">
+          Teach me more sentences! <q-icon color="red" name="fas fa-heart"/>
+        </q-tooltip>
+      </q-icon>
     </q-layout-footer>
+    <teach ref="teachingModal" />
   </q-layout>
 </template>
 
 <script>
-import { openURL } from 'quasar'
+import { openURL, QTooltip } from 'quasar'
+import Teach from '../components/Teach'
+
 export default {
   name: 'MyLayout',
+  components: {
+    QTooltip,
+    Teach
+  },
   data () {
     return {
       leftDrawerOpen: this.$q.platform.is.desktop
     }
   },
   methods: {
-    openURL
+    openURL,
+    test() {
+      console.log('test');
+    }
   }
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
+.fix {
+  margin: auto 0px;
+}
 </style>
