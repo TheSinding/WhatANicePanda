ce53d16c1577f63618fbd94602e6cfb0796f1656
Simon Sinding <sinding2000@gmail.com>
2018-08-20 20:13:14 +0200

    Vue social

---
+++ src/plugins/Vue-Social-Networks.js
@@ -0,0 +1,6 @@
+// import something here
+import SocialSharing from 'vue-social-sharing';
+// leave the export, even if you don't use it
+export default ({ app, router, Vue }) => {
+  Vue.use(SocialSharing);
+};
